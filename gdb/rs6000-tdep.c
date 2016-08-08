4b074f4b0f263914d79b2be9ae5746312cf8d86f
sangameshs <smallayy@in.ibm.com>
2016-08-08 16:59:42 +0530

    Display the entire stack traces for the binaries where stwu instruction to update the stack frame wasn't being read by gdb

--- gdb/rs6000-tdep.c
+++ gdb/rs6000-tdep.c
@@ -3331,6 +3331,9 @@ rs6000_frame_cache (struct frame_info *this_frame, void **this_cache)
 	}
     }
 
+  if (fdata.frameless && !fdata.nosavedpc)
+   fdata.frameless = 0;
+
   if (!fdata.frameless)
     {
       /* Frameless really means stackless.  */
